e2690c336e77198b5acba3de089cd5223677fd6c
Diana Vidal <dvidaal@gmail.com>
2023-01-09 19:53:42 +0100

    all files

--- main.js
+++ main.js
@@ -1,153 +1,56 @@
-let operacion1;
-let operacion2;
-let operacion3;
-let performance;
+window.addEventListener("load", () => {
+  const display = document.querySelector(".resultado");
+  const keypadButtons = document.getElementsByClassName("keypad-button");
 
-const iniciamosCalculadoraPRO = () => {
-  let showOperation = document.getElementById("showOperation");
-  let resultado = document.getElementById("resultado");
-  let reset = document.getElementById("reset");
-  let suma = document.getElementById("suma");
-  let minus = document.getElementById("minus");
-  let multi = document.getElementById("multi");
-  let divi = document.getElementById("divi");
-  let equal = document.getElementById("equal");
-  let root = document.getElementById("root");
-  let one = document.getElementById("one");
-  let two = document.getElementById("two");
-  let three = document.getElementById("three");
-  let four = document.getElementById("four");
-  let five = document.getElementById("five");
-  let six = document.getElementById("six");
-  let seven = document.getElementById("seven");
-  let eight = document.getElementById("eight");
-  let nine = document.getElementById("nine");
-  let cero = document.getElementById("cero");
-};
+  const keypadButtonsArray = Array.from(keypadButtons);
 
+  keypadButtonsArray.forEach((button) => {
+    button.addEventListener("click", () => {
+      calcula(button, resultado);
+    });
+  });
+});
 
-one.onclick = function () {
-  resultado.textContent = resultado.textContent + "1";
-};
-two.onclick = function () {
-  resultado.textContent = resultado.textContent + "2";
-};
-three.onclick = function () {
-  resultado.textContent = resultado.textContent + "3";
-};
-four.onclick = function () {
-  resultado.textContent = resultado.textContent + "4";
-};
-five.onclick = function () {
-  resultado.textContent = resultado.textContent + "5";
-};
-six.onclick = function () {
-  resultado.textContent = resultado.textContent + "6";
-};
-seven.onclick = function () {
-  resultado.textContent = resultado.textContent + "7";
-};
-eight.onclick = function () {
-  resultado.textContent = resultado.textContent + "8";
-};
-nine.onclick = function () {
-  resultado.textContent = resultado.textContent + "9";
-};
-cero.onclick = function () {
-  resultado.textContent = resultado.textContent + "0";
-};
-reset.onclick = function () {
-  resCalculator();
-};
-suma.onclick = function () {
-  operacion1 = resultado.textContent;
-  performance = "+";
-  showOperation.textContent = operacion1 + performance;
-  clearAll();
-};
-minus.onclick = function () {
-  operacion1 = resultado.textContent;
-  performance = "-";
-  showOperation.textContent = operacion1 + performance;
-  clearAll();
-};
-multi.onclick = function () {
-  operacion1 = resultado.textContent;
-  performance = "*";
-  showOperation.textContent = operacion1 + performance;
-  clearAll();
-};
-divi.onclick = function () {
-  operacion1 = resultado.textContent;
-  performance = "/";
-  showOperation.textContent = operacion1 + performance;
-  clearAll();
-};
-equal.onclick = function () {
-  operacion2 = resultado.textContent;
-  
-  showingResults();
-};
-
-root.onclick = function () {
-    operacion1 = resultado.textContent;
-    rootOperation();
-}
-
-punto.onclick = function () {
-    if(!resultado.textContent.includes(".")){
-        resultado.textContent = resultado.textContent + ".";
-    }
-}
-
-
-
-
-const clearAll = () => {
-  resultado.textContent = "";
-}
-
-const resCalculator = () => {
-  resultado.textContent = "";
-  operacion1 = 0;
-  operacion2 = 0;
-  performance = "";
-  showOperation.textContent = "";
- 
-}
-
-const rootOperation = () => {
-    resultado.textContent = parseFloat(Math.sqrt(operacion1).toFixed(3));
-}
-
-
-
-const showingResults = () => {
-  let res = 0;
-  switch (performance) {
-    case "+":
-      showOperation.textContent = operacion1 + "+" + operacion2;
-      res = parseFloat(operacion1) + parseFloat(operacion2) + parseFloat(operacion3);
+const calcula = (button, resultado) => {
+  switch (button.innerHTML) {
+    case "C":
+      borrar(resultado);
       break;
-
-    case "-":
-      showOperation.textContent = operacion1 + "-" + operacion2;
-      res = parseFloat(operacion1) - parseFloat(operacion2);
+    case "=":
+      resultadoTotal(resultado);
       break;
-
-    case "*":
-      showOperation.textContent = operacion1 + "*" + operacion2;
-      res = parseFloat(operacion1) * parseFloat(operacion2);
+    case "√":
+      rootOperation(resultado);
       break;
-
-    case "/":
-      showOperation.textContent = operacion1 + "/" + operacion2;
-      res = parseFloat(operacion1) / parseFloat(operacion2);
-     /*  if(parseFloat(operacion1) / 0){
-        alert("NO ES UN NÚMERO");
-      } */
+    case ".":
+      punto(resultado);
+      break;
+    default:
+      reset(resultado, button);
       break;
   }
-  resCalculator();
-  resultado.textContent = res;
-}
+};
+
+const punto = () => {
+  if (!resultado.textContent.includes("." || 0)) {
+    resultado.textContent = resultado.textContent + ".";
+  }
+};
+const resultadoTotal = (resultado) => {
+  resultado.innerHTML = parseFloat(eval(resultado.innerHTML).toFixed(3));
+};
+
+const reset = (resultado, button) => {
+  if (resultado.innerHTML === 0) {
+    resultado.innerHTML = "";
+  }
+  resultado.innerHTML += button.innerHTML;
+};
+
+const borrar = (resultado) => {
+  resultado.innerHTML = "";
+};
+
+const rootOperation = (button) => {
+  resultado.innerHTML = parseFloat(Math.sqrt(resultado.innerHTML).toFixed(3));
+};
